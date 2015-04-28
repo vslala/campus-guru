54b38929fb4ab58075d839f66d89b14056eb458b
= <varunshrivastava007@gmail.com>
2015-04-28 09:12:01 +0530

    After making minor change in the profileVisit page

--- resources/views/home/profileVisit.blade.php
+++ resources/views/home/profileVisit.blade.php
@@ -104,7 +104,29 @@
                     <div class="panel-body">
                 <div class="row">
     				<div class="col-md-12">
-                        @include('home._status')
+                    @if(count($user) > 0 && count($status) > 0)
+                    @foreach($status as $s)
+                            <ul class="nav nav-pills list-inline">
+                                <li>
+
+                                    <a href="{{ route("profileVisit", [
+                                        $user[0]['username']
+                                    ]) }}" >
+                                        {!! Html::image($userImage[0]['image_url'], $userImage[0]['image_name'], ['class'=>'img img-thumbnail img-responsive', 'style'=>'height: 100px;']) !!}
+                                    </a>
+
+
+                                </li>
+                                <li style="margin-top: 2%;"><span style="font-family: cursive,Lobster; font-weight: bold; color: #843534;">{{ $s->status or 'Status' }}</span>
+                                    <br>
+                                    <div class="help-block">created at: {{ $s->created_at }}</div>
+                      
+                                 </li>
+
+                                <hr>
+                            </ul>
+                    @endforeach
+                    @endif
     				</div>
 
                 </div>
