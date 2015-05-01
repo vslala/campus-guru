a0b4d0b6ecdc73b58c6ec1abbc11ff6953c58512
= <varunshrivastava007@gmail.com>
2015-05-02 00:08:59 +0530

    After adding cookie for visitors

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,7 +7,9 @@ use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\BlogView;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
 
 class BlogController extends Controller {
 
@@ -64,25 +66,32 @@ class BlogController extends Controller {
             ->orderBy("blog_comments.created_at", "asc")
             ->get();
 
+
         if(isset(Auth::user()->username))
-            $username = Auth::user()->username;
-        else
-            $username = 'vister_'.uniqid();
+            Session::put('username', Auth::user()->username);
+        else if(Cookie::get('VISITOR')){
+            Session::put('username', Cookie::get('VISITOR'));
+        }
+        else{
+            Session::put('username', 'vister_'.uniqid() );
+            $cookie = Cookie::make("VISITOR", uniqid(), 7200);
+        }
+
 
         /*
          * Increment Blog Views
          */
-        $blogViews = BlogView::where(["blog_id"=>$id, 'username'=>$username])->first();
+        $blogViews = BlogView::where(["blog_id"=>$id, 'username'=>Session::get('username')])->first();
         if(count($blogViews) <= 0){
             $b = new BlogView();
             $b->blog_id = $id;
-            $b->username = $username;
+            $b->username = Session::get('username');
             $b->total_views = 1;
             if($b->save()){
                 $total_views = $b->totalViews($id);
             }else {
                 $total_views = $b->totalViews($id);
-                return view('home.showBlog', compact('blog','userComments','total_views','username'));
+                return view('home.showBlog', compact('blog','userComments','total_views','username','cookie'));
             }
 
 
