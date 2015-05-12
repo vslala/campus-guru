c739c3de830db9d70ebe49c96698f8de29e1d394
= <varunshrivastava007@gmail.com>
2015-05-12 11:30:55 +0530

    Profile Visit Changes

--- resources/views/home/_status.blade.php
+++ resources/views/home/_status.blade.php
@@ -2,28 +2,26 @@
 @foreach($status as $s)
 <ul class="nav nav-pills list-inline">
                     <div class="panel-heading">
-                    <li>
+                        <li>
 
-                                                        <a href="{{ route("profileVisit", [
-                                                            $user[0]['username']
-                                                        ]) }}" >
-                                                            {!! Html::image($userImage[0]['image_url'], $userImage[0]['image_name'], ['class'=>'img img-thumbnail img-responsive', 'style'=>'height: 100px;']) !!}
-                                                        </a>
+                          <a href="{{ route("profileVisit", [
+                              $user[0]['username']
+                              ]) }}" >
+                          {!! Html::image($userImage[0]['image_url'], $userImage[0]['image_name'], ['class'=>'img img-thumbnail img-responsive', 'style'=>'height: 100px;']) !!}
+                          </a>
 
 
-                                                    </li>
+                        </li>
                     </div>
-
-
                     <div class="panel-body">
                         <li style="margin-top: 2%;"><span style="font-family: cursive,Lobster; font-weight: bold; color: #843534;">{{ $s->status or 'Status' }}</span>
-                                                        <br>
-                                                        <div class="help-block">created at: {{ $s->created_at }}</div>
-                                                        <div class="help-block">
-                                                            <span class="pull-left"><a href="{{ route('deleteStatus', $s->id) }}">delete</a></span>
-                                                        </div>
-                                                     </li>
-                    </div>               {{--<li class="nav-divider"></li>--}}
+                        <br>
+                            <div class="help-block">created at: {{ $s->created_at }}</div>
+                                 <div class="help-block">
+                                       <span class="pull-left"><a href="{{ route('deleteStatus', $s->id) }}">delete</a></span>
+                                 </div>
+                        </li>
+                    </div>
 
 
                                 <hr>
--- resources/views/home/profileVisit.blade.php
+++ resources/views/home/profileVisit.blade.php
@@ -109,21 +109,27 @@
                     @if(count($user) > 0 && count($status) > 0)
                     @foreach($status as $s)
                             <ul class="nav nav-pills list-inline">
-                                <li>
+                                <div class="panel-heading">
+                                    <li>
 
-                                    <a href="{{ route("profileVisit", [
-                                        $user[0]['username']
-                                    ]) }}" >
-                                        {!! Html::image($userImage[0]['image_url'], $userImage[0]['image_name'], ['class'=>'img img-thumbnail img-responsive', 'style'=>'height: 100px;']) !!}
-                                    </a>
+                                        <a href="{{ route("profileVisit", [
+                                            $user[0]['username']
+                                        ]) }}" >
+                                            {!! Html::image($userImage[0]['image_url'], $userImage[0]['image_name'], ['class'=>'img img-thumbnail img-responsive', 'style'=>'height: 100px;']) !!}
+                                        </a>
 
 
-                                </li>
-                                <li style="margin-top: 2%;"><span style="font-family: cursive,Lobster; font-weight: bold; color: #843534;">{{ $s->status or 'Status' }}</span>
-                                    <br>
-                                    <div class="help-block">created at: {{ $s->created_at }}</div>
+                                    </li>
+                                </div>
+                                <div class="panel-body">
+                                    <li style="margin-top: 2%;"><span style="font-family: cursive,Lobster; font-weight: bold; color: #843534;">{{ $s->status or 'Status' }}</span>
+                                           <br>
+                                           <div class="help-block">created at: {{ $s->created_at }}</div>
 
-                                 </li>
+                                    </li>
+                                </div>
+
+
 
                                 <hr>
                             </ul>
