f77269ef36494dad941d161aa1b7ec3ca5aec7ad
= <varunshrivastava007@gmail.com>
2015-05-14 17:54:30 +0530

    Notification for messages

--- app/SendMessage.php
+++ app/SendMessage.php
@@ -15,7 +15,14 @@ class SendMessage extends Model {
         $m->file_name = $fileName;
         $m->file_url = $fileUrl;
         $flag = $m->save();
+        $message_id =  $m->id;
         if($flag){
+            $n = new Notification();
+            $n->n_to = $reciever;
+            $n->n_by = $sender;
+            $n->n_for = 3;
+            $n->n_id_of = $message_id;
+            $n->save();
             return true;
         }else{
             return false;
